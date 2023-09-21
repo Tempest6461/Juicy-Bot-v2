528b1f391559cb051478367046cd61ff0b9a32c3
Alexander Compton <116861252+Tempest6461@users.noreply.github.com>
2023-09-21 00:53:36 -0400

    Listcustomcmds 2

--- command-handler/command-handler/commands/listcustomcmds.js
+++ command-handler/command-handler/commands/listcustomcmds.js
@@ -10,19 +10,21 @@ module.exports = {
     permissions: [PermissionFlagsBits.Administrator],
   
     callback: async ({ instance, guild }) => {
-      const customCommands = await instance.commandHandler._customCommands.getAll(guild.id);
+      const customCommands = await instance.commandHandler.customCommands.get(guild.id);
   
-      if (customCommands.length === 0) {
+      if (!customCommands || !customCommands.length) {
         return {
-          content: "There are no custom commands in this server.",
+          content: "There are no custom commands in this server",
           ephemeral: true,
         };
       }
-  
-      const commandList = customCommands.map((cmd) => `- ${cmd.name}: ${cmd.description}`).join('\n');
-  
+
+      const commands = customCommands.map((command) => {
+        return `\`${command.commandName}\` - ${command.description}`;
+      });
+
       return {
-        content: `Custom Commands:\n${commandList}`,
+        content: commands.join("\n"),
         ephemeral: true,
       };
     },
