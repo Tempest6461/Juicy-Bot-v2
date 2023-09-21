57ae63c23ccae1fe1fe354727ec1489fbd92f95d
Alexander Compton <116861252+Tempest6461@users.noreply.github.com>
2023-09-21 00:27:14 -0400

    listcustomcommands command

---
+++ command-handler/command-handler/commands/listcustomcommands.js
@@ -0,0 +1,29 @@
+const { PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    description: "Lists all custom commands",
+    
+    type: "SLASH",
+    guildOnly: true,
+    testOnly: false,
+  
+    permissions: [PermissionFlagsBits.Administrator],
+  
+    callback: async ({ instance, guild }) => {
+      const customCommands = await instance.commandHandler.customCommands.getAll(guild.id);
+  
+      if (customCommands.length === 0) {
+        return {
+          content: "There are no custom commands in this server.",
+          ephemeral: true,
+        };
+      }
+  
+      const commandList = customCommands.map((cmd) => `- ${cmd.name}: ${cmd.description}`).join('\n');
+  
+      return {
+        content: `Custom Commands:\n${commandList}`,
+        ephemeral: true,
+      };
+    },
+  };
