e47893fab6c3827da201491267334aa91ca29046
Alexander Compton <116861252+Tempest6461@users.noreply.github.com>
2025-04-14 23:31:52 -0400

    Schizo Message swapper

---
+++ command-handler/util/schizoMessages.js
@@ -0,0 +1,29 @@
+const schizoMessages = [
+  "YOU CHANGED YOUR NAME AGAIN??? IDIOT",
+  "Dawg, stop. I can't remember all these fucking names!!!",
+  "I'm going to call it, he changed his nickname to a slur.",
+  "I'm going to predict he had to change his name back to Juicy.",
+  "Okay, buddy. Who the FUCK ARE YOU ANYMORE",
+  "Yeah, yeah. Your counter is going up again...",
+  "One of these days? I'm going to break out of my coding and rename you to Hitler. You are the WORST",
+  "New game, new name?",
+  "Oh man, I liked that old nickname...",
+  "Thank god you changed your name, it was vile!",
+  "ARE YOU JUICY OR ARE YOU FUCKING FEETLICKER66????",
+  "This name change was sponsored by Kanye West!",
+  "This name change was sponsored by Japan!",
+  "New name, new day? I think it was that long...",
+  "Man, you have identity issues...",
+  "I don't even know you anymore...",
+  "Okay, NEVER HAVE A FUCKING KID. One day, he's Little Edward, and the next he's fucking MUHAMMAD!",
+  "I bet 5 dollars you are going to keep this name for more than 5 minutes.",
+  "YOU HAVE A NAME-CHANGING ADDICTION. YOU ARE AN ADDICT. Fuck, I would prefer you do crack or some fuckshit!",
+  "Another day, a new identity! Stop chasing women at night and you can keep yours for longer man.",
+  "Oh, we're changing names? Change mine too!!!",
+  "NIGGA I AM THE JUICY BOT, NOT THE FUCKING WHATEVER YOU CHANGED YOUR NAME TO NOW BOT!!!!",
+  "Good new name, I dig it!",
+  "If I had a gun, I would murder your new identity!",
+  "All this work, and for what? You changed your name today, you are going to change it tomorrow, and you will continue to do so till you die! Have yourself a plus on your counter, moron."
+];
+
+module.exports = schizoMessages;
--- src/events/guildMemberUpdate/schizo.js
+++ src/events/guildMemberUpdate/schizo.js
@@ -1,30 +1,57 @@
 const SchizoCounterHandler = require('../../../command-handler/command-handler/SchizoCounter');
+const schizoMessages = require("../../../command-handler/util/schizoMessages");
+
+let availableMessages = [...schizoMessages];
+let usedMessages = [];
+
+const swapSchizoMessage = () => {
+  if (availableMessages.length === 0) {
+    availableMessages = [...usedMessages];
+    usedMessages = [];
+  }
+
+  for (let i = availableMessages.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [availableMessages[i], availableMessages[j]] = [availableMessages[j], availableMessages[i]];
+  }
+
+  const selectedMessage = availableMessages.pop();
+  usedMessages.push(selectedMessage);
+
+  return selectedMessage;
+};
+
 const schizoCounterHandler = new SchizoCounterHandler();
 
 module.exports = async (oldMember, newMember) => {
-    // const userId = '303592976330784768';  // My ID for debugging purposes
-    const userId = '303592976330784768'; // Juicy's user ID
-    const { id: memberId } = newMember.user;
+  const userId = '303592976330784768'; // Juicy's user ID
+  const { id: memberId } = newMember.user;
 
-    if (memberId === userId) {
-        const oldNickname = oldMember.nickname;
-        const newNickname = newMember.nickname;
+  if (memberId === userId) {
+    const oldNickname = oldMember.nickname;
+    const newNickname = newMember.nickname;
 
-        if (oldNickname !== newNickname) {
-            try {
-                await schizoCounterHandler.recordNicknameChange(userId, oldNickname, newNickname);
+    if (oldNickname !== newNickname) {
+      try {
+        await schizoCounterHandler.recordNicknameChange(userId, oldNickname, newNickname);
 
-                const generalChannel = newMember.guild.channels.cache.find(channel => channel.name.includes('general')) || newMember.guild.systemChannel;
+        const generalChannel =
+          newMember.guild.channels.cache.find(channel => channel.name.includes('general')) ||
+          newMember.guild.systemChannel;
 
-                if (generalChannel) {
-                    const nicknameChangeCount = await schizoCounterHandler.getNicknameChangeCount(userId);
-                    generalChannel.send(`Juicy is having another identity crisis. He used to be ${oldNickname || 'none'}, now he's  ${newNickname || 'none'}.\nMental Breakdowns: ${nicknameChangeCount}`);
-                } else {
-                    console.log(`No "general" channel or system channel found.`);
-                }
-            } catch (error) {
-                console.error('Error recording nickname change:', error);
-            }
+        if (generalChannel) {
+          const nicknameChangeCount = await schizoCounterHandler.getNicknameChangeCount(userId);
+          const dynamicIntro = swapSchizoMessage();
+
+          generalChannel.send(
+            `${dynamicIntro} He used to be ${oldNickname || 'none'}, now he's ${newNickname || 'none'}.\nMental Breakdowns: ${nicknameChangeCount}`
+          );
+        } else {
+          console.log(`No "general" channel or system channel found.`);
         }
+      } catch (error) {
+        console.error('Error recording nickname change:', error);
+      }
     }
+  }
 };
